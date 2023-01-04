386ddb440202b8a0e1178ed0888c929f1e60dcbf
jiazhiyuan <jiazhiyuan4564@sina.com>
2023-01-04 11:33:30 +0800

    Initial commit

---
+++ src/main/java/com/example/metricsView/MetricsViewApplication.java
@@ -0,0 +1,13 @@
+package com.example.metricsView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsViewApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/metricsView/controller/DataEngineInfoController.java
@@ -0,0 +1,8 @@
+package com.example.metricsView.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DataEngineInfoController {
+
+}
